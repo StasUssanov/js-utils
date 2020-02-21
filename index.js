769fc702ae07d23228d3bea64a9b8b5974fb471b
StasUssanov <s.ussanov@mail.ru>
2020-02-21 17:38:48 +0600

    feat(utils) доб функцию arrayUnique

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as explode } from "./explode";
 export { default as getJwtData } from "./get-jwt-data";
 export { default as flatToHierarchy } from "./flat-to-hierarchy";
+export { default as arrayUnique } from "./array-unique";
