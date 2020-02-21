abc53c30c54cb62ae2727bd53109d2788c91342e
StasUssanov <s.ussanov@mail.ru>
2020-02-21 17:47:02 +0600

    feat(utils) доб функции getDate и getDateDecrement

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ export { default as getJwtData } from "./get-jwt-data";
 export { default as flatToHierarchy } from "./flat-to-hierarchy";
 export { default as arrayUnique } from "./array-unique";
 export { default as getRandom } from "./get-random";
+export { getDate } from "./get-date";
+export { getDateDecrement } from "./get-date";
