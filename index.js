256f139154d66278b9c9b1b8bbcc737770354e44
StasUssanov <s.ussanov@mail.ru>
2020-02-21 17:41:38 +0600

    feat(utils) доб функцию getRandom

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export { default as explode } from "./explode";
 export { default as getJwtData } from "./get-jwt-data";
 export { default as flatToHierarchy } from "./flat-to-hierarchy";
 export { default as arrayUnique } from "./array-unique";
+export { default as getRandom } from "./get-random";
